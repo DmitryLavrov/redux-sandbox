f620ba8dcff5fcd607c9e6e55023935a501085a7
DmitryLavrov <d3236562@mail.ru>
2021-05-31 08:54:11 +0300

    Payload and Action creators are realized

--- README.md
+++ README.md
@@ -30,6 +30,16 @@ console.log(state)
 state = reducer(state, {type: 'INC'})
 console.log(state)
 ```
+___
 Reducer must be a `pure function`:
 * The function return values are identical for identical arguments
 * The function application has no side effects
+___
+Action creators:
+```javascript
+const dec = () => ({type: 'DEC'})
+const rnd = (payload) => ({type: 'RND', payload})
+
+store.dispatch(dec())
+store.dispatch(rnd(payload))
+```
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import {createStore} from 'redux'
 
+const inc = () => ({type: 'INC'})
+const dec = () => ({type: 'DEC'})
+const rnd = (payload) => ({type: 'RND', payload})
+
+
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1
     case 'DEC':
       return state - 1
+    case 'RND':
+      return state + action.payload
     default:
       return state
   }
@@ -16,12 +23,21 @@ const store = createStore(reducer)
 document
   .getElementById('dec')
   .addEventListener('click',
-    () => store.dispatch({type: 'DEC'}))
+    () => store.dispatch(dec()))
 
 document
   .getElementById('inc')
   .addEventListener('click',
-    () => store.dispatch({type: 'INC'}))
+    () => store.dispatch(inc()))
+
+document
+  .getElementById('rnd')
+  .addEventListener('click',
+    () => {
+      const payload = Math.floor(Math.random() * 10)
+      console.log(payload)
+      store.dispatch(rnd(payload))
+    })
 
 const update = () => {
   document
